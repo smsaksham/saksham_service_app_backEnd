5b2b4ca94f403b238d4108a99a07644ca8029cd2
smsaksham <msaksham8120@gmail.com>
2025-03-21 17:47:09 +0530

    .ENV FILE UPDATE DATABASE URL CHANGE

--- connnection/db.connection.js
+++ connnection/db.connection.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
-const url = process.env.URL ||  "mongodb://127.0.0.1:27017/service_app"
+const url = process.env.URL ||  "mongodb+srv://sakshammalviya12345:@saksham8120@cluster0.l3njz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+// 
 mongoose.connect(url)
 console.log("Connect to Database to server");
